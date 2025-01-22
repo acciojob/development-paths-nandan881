9702da1ca483a0c99bb42633d5e9a5b4e8087697
Nandan kumar <kumarnandan551@gmail.com>
2025-01-22 14:20:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+it('should display correct paragraph text', () => {
+  cy.visit('path_to_your_form.html');  // Ensure this is the correct path
+
+  // Update the test to expect the colon at the end
+  cy.get('p').should('have.text', '5 software program developer profession paths:');
+});
